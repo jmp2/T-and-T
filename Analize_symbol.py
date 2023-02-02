e7716e7a2e1dd593440f4c1a3305ca34df2d6a96
segaru23 <segaru23@gmail.com>
2023-02-02 22:51:19 +0100

    lo que te apetezca

--- Analize_symbol.py
+++ Analize_symbol.py
@@ -8,20 +8,50 @@ Created on Tue Dec 27 12:39:37 2022
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
+import numpy as np
+from datetime import datetime as dt
 
 
+class Analize_symbol():
 
-a_file = open("AAL.pkl", "rb")
-symbol = pickle.load(a_file)    
-a_file.close()    
+    @staticmethod
+    def create_symbol_dataset (symbol):
+    
+    
+        data = pd.read_pickle("./"+symbol)
+        
+                    
+        df = pd.DataFrame(data[1:],columns=data[0][:])
+        df_ = df[df["time"]!="time"]
+        formato = "%Y-%m-%d %H:%M:%S"
+        df_["time"] = pd.to_datetime(df_['time'], format=formato)
+        
+        df_.set_index(pd.DatetimeIndex(df_["time"]), inplace=True)
+        df_.sort_index(inplace=True)
+        df_["open"] = df_["open"].astype(float)
+        df_["high"] = df_["high"].astype(float)
+        df_["low"] = df_["low"].astype(float)
+        df_["close"] = df_["close"].astype(float)
+        df_["unix"] = pd.to_datetime(df_['time']).astype(np.int64)/10**9
+        
+        df_["hour"] = df_["time"].dt.time
+        start_time = "09:30:00"
+        final_time = "16:00:00"
+        format = "%H:%M:%S"
+        start_time = "09:30:00"
+        final_time = "16:00:00"
+        start_time = dt.strptime(start_time, format)
+        final_time = dt.strptime(final_time, format)
+        df_ = df_[df_["hour"]>=start_time.time()]
+        df_ = df_[df_["hour"]<=final_time.time()]
+        df_["time_difference"] = df_["unix"].diff()
+    
+        
+        return df_
     
 
-
-df = pd.DataFrame(symbol)
-df.columns = symbol[0]
-df = df[df.time != 'time']
-df.time = pd.to_datetime(df.time, format='%Y-%m-%d %H:%M:%S.%f')
+    @staticmethod
+    def Represent_symbol (columns):
+        
 
 
-plt.plot(df.time,df.close)
-plt.show()
--- Main.py
+++ Main.py
@@ -3,67 +3,41 @@
 Created on Sat Oct 15 22:44:48 2022
 
 @author: segar
+
+
+Modes:
+    1- Take and clean Data from Alpha Vantage Api
+    2- Analize data, add indicators
+    3- Simulate strategy
+    4- Optimize strategy
+    5- Real time signals
+
+
+
 """
 
-from datetime import datetime
-import requests
-import numpy as np
-import json
-import matplotlib.pyplot as plt
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from Analize_symbol import Analize_symbol, Represent_symbol
+import Calculate_indicator 
 
-#https://query1.finance.yahoo.com/v8/finance/chart/%5EIXIC?symbol=%5EIXIC&period1=1664316000&period2=1664920800&useYfid=true&interval=1m&includePrePost=true&events=div%7Csplit%7Cearn&lang=es-ES&region=ES&crumb=SwbdaQw9MYL&corsDomain=es.finance.yahoo.com
-#https://query1.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=1667943451&period2=1668116251&useYfid=true&interval=1m&includePrePost=true&events=div|split|earn&lang=es-ES&region=ES&crumb=SwbdaQw9MYL&corsDomain=es.finance.yahoo.com
-
-date_string_1 = "1 December, 2022"
-date_string_2 = "2 December, 2022"
+mode = 2
+args = "AAL.pkl"
 
 
-date_object = datetime.strptime(date_string_1, "%d %B, %Y")
-timestamp_1 = int(datetime.timestamp(date_object))
-date_object = datetime.strptime(date_string_2, "%d %B, %Y")
-timestamp_2 = int(datetime.timestamp(date_object))
-# timestamp_2_2 = timestamp_1 + 604800
 
-# resp = requests.get('https://tvc4.investing.com/25bb9d3d09dcde4f698aae33d569f7a7/1668119234/4/4/58/history?symbol=6408&resolution=1&from=1651096800&to=1651183200')
+def mode_1():
+    df = Analize_symbol.create_symbol_dataset(args)
+    print(df)
+    
 
-
-# d = np.load('Nasdaq1week.txt').tojson
-
-# import json
-  
-# # Opening JSON file
-# f = open('Nasdaq1week.txt')
-  
-# # returns JSON object as 
-# # a dictionary
-# data = json.load(f)
-# f.close()
-#%%
+def mode_2():    
+    df = Calculate_indicator.Calculate_indicator.compute_rsi(df)
+    Analize_symbol.represent_symbol("close","rsi_close",period_ini = "01-01-2022", perid_end = "01-02-2022")
+    
 
 
-# Definimos la URL-principal y la ruta al driver de chrome
-main_url = 'https://query1.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=' + str(timestamp_1) + '&period2=' + str(timestamp_2) + '&useYfid=true&interval=1m&includePrePost=true&events=div|split|earn&lang=es-ES&region=ES&crumb=SwbdaQw9MYL&corsDomain=es.finance.yahoo.com' 
-chromedriver = './chromedriver'
-# Abrimos una ventana con la URL-principal
-driver= webdriver.Chrome(chromedriver)
-driver.get(main_url)
-Body = driver.find_element(By.XPATH, "/html/body").text
+switch_mode = {
+    1: mode_1,
+    2: mode_2
+        }
 
-Body_json = json.loads(Body)
-
-price_high = np.array(Body_json["chart"]["result"][0]["indicators"]["quote"][0]["high"],'float')
-price_low = np.array(Body_json["chart"]["result"][0]["indicators"]["quote"][0]["low"],'float')
-price_open = np.array(Body_json["chart"]["result"][0]["indicators"]["quote"][0]["open"],'float')
-price_close = Body_json["chart"]["result"][0]["indicators"]["quote"][0]["close"]
-volume = np.array(Body_json["chart"]["result"][0]["indicators"]["quote"][0]["volume"],'float')
-timestamp = Body_json["chart"]["result"][0]["timestamp"]
-time.sleep(2)
-driver.close()
-
-plt.plot(timestamp, price_close)
-plt.show()
-
-
+switch_mode.get(mode)
