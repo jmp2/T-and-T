a0fd7036295849bd44fbc0cdf05a23036b13b38e
jorge <jorgemprats97@gmail.com>
2023-04-18 21:06:46 +0200

    fix(#1): Fixed error saving report file

--- src/report.py
+++ src/report.py
@@ -2,7 +2,7 @@
 
 class Report():
     
-    def __init__(self, save_report=False, file_path="report/") -> None:
+    def __init__(self, save_report=False, file_path="reports/") -> None:
         self.report_metrics = {}
         self.save_report = save_report
         self.file_path = file_path
@@ -102,10 +102,13 @@ class Report():
         report += f"Initial budget: {self.report_metrics['initial_budget']}\n"
         report += f"Number of operations: {self.report_metrics['n_operations']}\n"
         report += f"Final budget: {self.report_metrics['final_budget']}\n"
+        print(self.report_metrics["strategy"].get_name())
+        print(str(self.report_metrics['init_period']))
         report += f"Performance: {self.report_metrics['final_budget'] / self.report_metrics['initial_budget']}\n"
-        filename_report = self.file_path +"_"+str(self.report_metrics["strategy"]) \
-                            +"_"+str(self.report_metrics['init_period'])+"_" \
-                            + str(self.report_metrics['init_period'])
-        with open(filename_report, "w") as f:
+        filename_report = self.file_path+str(self.report_metrics["strategy"].get_name()) \
+                            +"_"+str(self.report_metrics['init_period'])[0:9]+"_" \
+                            + str(self.report_metrics['final_period'])[0:9]+".txt"
+        print(filename_report)
+        with open(filename_report,'x') as f:
             f.write(report)
 
