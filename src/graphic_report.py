f751667e43c097d6d2fd04bb14646ff396b5a143
jorge <jorgemprats97@gmail.com>
2023-04-29 20:04:23 +0200

    feat(#1): Added comparison graph between index and strategy returns

--- src/graphic_report.py
+++ src/graphic_report.py
@@ -4,6 +4,7 @@ import plotly.io as pio
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 pio.renderers.default='browser'
 
@@ -12,6 +13,18 @@ class GraphicReport():
     def __init__(self) -> None:
         self.data = None
         self.position_list = None
+        self.price = []
+        self.current_budget = []
+
+    def append_record(self, price, position):
+        self.price.append(price)
+        self.current_budget.append(position)
+
+    def compute_comparison(self):
+        df_comp = pd.DataFrame()
+        df_comp["price"] = self.price / self.price[0]
+        df_comp["position"] = self.current_budget / self.current_budget[0]
+        return df_comp
 
     def set_data(self, data):
         self.data = data
@@ -37,13 +50,19 @@ class GraphicReport():
         fig.show()
 
     def seaborn_plot(self, cols):
-        fig, axes = plt.subplots(2, 1, figsize=(18, 10))
+        _, axes = plt.subplots(3, 1, figsize=(18, 10))
         g = sns.lineplot(ax=axes[0], data=self.data, x=self.data.index, y="close")
         for pos in self.position_list:
             g.axvline(x=pos.open_date, color="green")
             g.axvline(x=pos.close_date, color="red")
         for col in cols:
-            sns.lineplot(ax=axes[1], data=self.data, x=self.data.index,
+            sns.lineplot(ax=axes[1], data=self.data,
+                         x=self.data.index,
                          y=col, label=col)
+        df_comp = self.compute_comparison()
+        sns.lineplot(ax=axes[2], data=df_comp, x=self.data.index,
+                     y="price", label="Index returns")
+        sns.lineplot(ax=axes[2], data=df_comp, x=self.data.index,
+                     y="position", label="Strategy returns")
         plt.legend()
         plt.show()
